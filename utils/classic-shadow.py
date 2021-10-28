fad6666bd4ce91afed2b7b98a728e279b307f521
Greg Stroot <gstroot@caltech.edu>
2021-10-27 17:44:24 -0700

    Added an outline for classical shadow approach

--- utils/classic-shadow.py
+++ utils/classic-shadow.py
@@ -2,3 +2,37 @@
 This file will implement classic shadow that takes input rho, output rho tilde
 '''
 
+import numpy as np
+
+
+def classical_shadow_generation():
+    """
+    Summary:
+        Implementation of classical shadow approach follow the approach of 
+
+         Hsin-Yuan Huang, Richard Kueng, and John Preskill.
+         Predicting many properties of a quantumsystem from very few measurements.
+         Nature, 16, 2020.
+
+    Inputs:
+        var1        - Type:
+                    - 
+        var1        - Type:
+                    - 
+        var1        - Type:
+                    - 
+    Outputs:
+        var1        - Type:
+                    - 
+        var1        - Type:
+                    - 
+    """
+
+
+    return
+
+
+
+if __name__ == '__main__':
+    #Testing of funciton here
+    pass
